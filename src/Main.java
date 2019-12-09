3f199827c16daef9e14e4fae89631d75b50c5712
nurelayen <TPCF235018UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-12-09 10:35:30 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        String myVar ="Nuri";
+
+        System.out.println("HELLO WORLD!");
+        System.out.println(myVar);
+        System.out.println("Hello and Welcome, "+myVar);
+
+    }
+
+}
